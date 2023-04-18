5669b9a7a070e24cad1cc62434ce3f8a03491c5a
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-18 17:46:13 +0100

    Imported the link to connect to the db

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const Router = require("./routes")
 const app = express();
 
 app.use(express.json());
+
+const MONGODB_URI = "mongodb+srv://mongoose-learning-curve:Start123@@cluster0.fy0avua.mongodb.net/mongooseLearningCurve?retryWrites=true&w=majority"
