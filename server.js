cab23ce5142a3e11b19ff03f3ab7c9dd5a527cf9
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-18 17:58:49 +0100

    Connected mongoose to the database

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ const app = express();
 
 app.use(express.json());
 
-const MONGODB_URI = "mongodb+srv://mongoose-learning-curve:Start123@@cluster0.fy0avua.mongodb.net/mongooseLearningCurve?retryWrites=true&w=majority"
+const MONGODB_URI = "mongodb+srv://mongoose-learning-curve:Start123@@cluster0.fy0avua.mongodb.net/mongooseLearningCurve?retryWrites=true&w=majority" 
+
+mongoose.connect(MONGODB_URI)
+    .then((result) => {
+        console.log("Connected to the database successfully");
+    })
+   
