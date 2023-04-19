f873ddd3f2d5831da00e16be1c9a15959c2406d8
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-19 05:30:28 +0100

    Created a user collection and document

--- server.js
+++ server.js
@@ -8,8 +8,21 @@ app.use(express.json());
 
 const MONGODB_URI = "mongodb+srv://mongoose-learning-curve:Start123@cluster0.fy0avua.mongodb.net/mongooseLearningCurve?retryWrites=true&w=majority" 
 
+// Connecting to the database
 mongoose.connect(MONGODB_URI)
     .then((result) => {
         console.log("Connected to the database successfully");
     })
     .catch(err => {err.message})
+
+// Creating a schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    age: {
+        type: Number,
+        default: 0,
+    }
+});
